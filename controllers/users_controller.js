edfbfeef2094e0be278e630a8ef25ed2b622d490
Aman Gupta <amang5840@gmail.com>
2021-03-19 22:56:33 +0530

    create routes for users profile and posts

---
+++ controllers/users_controller.js
@@ -0,0 +1,7 @@
+module.exports.profile = (req, res) => {
+    return res.end("<h1>Profile</h1>");
+};
+
+module.exports.posts = (req, res) => {
+    return res.end("<h1>Posts</h1>");
+};
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ const homeController = require('../controllers/home_controller');
 console.log("Router loaded");
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further routes
+// router.use('/routernme', require('./routefile'));
+
 module.exports = router;
