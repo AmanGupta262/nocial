d78e6eeb144612cfe74ba214517833a633c32f1e
Aman Gupta <amang5840@gmail.com>
2021-03-19 23:00:05 +0530

    setup view engine i.e. ejs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // use router
 app.use('/', require('./routes'));
 
+// setup view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, (err) => {
     if(err){
         console.log(`Error in starting server ${err}`);
