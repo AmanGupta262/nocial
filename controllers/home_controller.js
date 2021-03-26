cf51f1a500c6280f7d951596c1120e9884c49a19
Aman Gupta <amang5840@gmail.com>
2021-03-26 11:17:24 +0530

    display post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,15 @@
-module.exports.home = function(req, res){
-    return res.render('home', {title: "Home"});
+const Post = require('../models/post')
+const User = require('../models/user')
+
+module.exports.home = function (req, res) {
+    Post.find({}).populate('user').exec((err, posts) => {
+        if (err) { console.log("Error in finding posts"); return; }
+
+        return res.render('home', {
+            title: "Home",
+            posts: posts
+        });
+    });
 };
 
 //module.exports.actionName = (req, res) => {}
