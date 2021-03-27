5bb1a43f6ee3f58b83ee55ae8e4faa36bea6b209
Aman Gupta <amang5840@gmail.com>
2021-03-27 12:01:46 +0530

    create comment schema

---
+++ models/comment.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+},{
+    timestapms: true
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
