529e6d127878727eb8edb18891399dcc4a13c0e3
Aman Gupta <amang5840@gmail.com>
2021-03-27 16:14:38 +0530

    read comments form database

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,16 @@
 const Post = require('../models/post')
-const User = require('../models/user')
 const timeago = require('timeago.js');
 
 module.exports.home = function (req, res) {
     Post.find({})
-    .populate('user').exec((err, posts) => {
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec((err, posts) => {
         if (err) { console.log("Error in finding posts"); return; }
 
         return res.render('home', {
