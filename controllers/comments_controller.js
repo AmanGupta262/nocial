b7546f06b1a07de3c01b551d071a20dd3fda6e40
Aman Gupta <amang5840@gmail.com>
2021-03-30 17:11:28 +0530

    delete comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -19,3 +19,23 @@ module.exports.addComment = (req, res) => {
     });
     
 };
+
+module.exports.destroy = (req, res) => {
+    Comment.findById(req.params.id, (err, comment) => {
+        if (err) { console.log("Error in finding comment"); return; }
+        
+        if(comment.user == req.user.id){
+            let postId = comment.post;
+
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}, (err, post) => {
+                if (err) { console.log("Error in finding post"); return; }
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
+};
--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ const passport = require('passport')
 const commentssController = require('../controllers/comments_controller');
 
 router.post('/create', passport.checkAuthenticated, commentssController.addComment);
+router.get('/destroy/:id', passport.checkAuthenticated, commentssController.destroy);
 
 module.exports = router;
