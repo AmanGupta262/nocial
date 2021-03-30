ae0c4997a22a8ad54bdd039c77e123d12aaa58da
Aman Gupta <amang5840@gmail.com>
2021-03-30 16:44:16 +0530

    delete posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.createPost = (req, res) => {
     Post.create({
@@ -6,8 +7,23 @@ module.exports.createPost = (req, res) => {
         user: req.user._id
     },
     (err, post) => {
-        if(err){ console.log("Error in creaatin post"); return; }
+        if(err){ console.log("Error in creating post"); return; }
 
         return res.redirect('back');
     });
 };
+
+module.exports.destroy = (req, res) => {
+    Post.findById(req.params.id, (err, post) => {
+        if (err) { console.log("Error in finding post"); return; }
+
+        // .id means converting the object id to string
+        if( post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, (err) => {
+                return res.redirect('back');
+            });
+        }
+    });
+}
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,6 @@ const passport = require('passport')
 const postsController = require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthenticated, postsController.createPost);
+router.get('/destroy/:id', passport.checkAuthenticated, postsController.destroy);
 
 module.exports = router;
