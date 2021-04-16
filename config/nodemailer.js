22dd82e11a9642959b878c8d72d7b50337376fa2
Aman Gupta <amang5840@gmail.com>
2021-04-16 23:20:17 +0530

    render email template

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -17,7 +17,7 @@ let transporter = nodemailer.createTransport({
 let renderTemplate = (data, relativePath) => {
     let mailHtml;
     ejs.renderFile(
-        path.join(__dirname, '../views/mailer/', relativePath),
+        path.join(__dirname, '../views/mailers/', relativePath),
         data,
         function(err, template){
             if(err){ console.log("Error in rendering template: ", err); return;}
--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,12 +1,14 @@
 const nodemailer = require('../config/nodemailer');
 
 exports.newComment = (comment) => {
+    let htmlString = nodemailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
     console.log("Inside new comment mailer");
     nodemailer.transporter.sendMail({
         from: 'nocial.com',
         to: comment.user.email,
         subject: "New Comment Published",
-        html: '<h1> Your comment is now Published </h1>'
+        html: htmlString
     },
     (err, info) => {
         if(err){ console.log('Error in sending mail: ',err); return; }
