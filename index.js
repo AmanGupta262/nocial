67e78e6053ca29d7fdc6609483e942366b5ec8c6
Aman Gupta <amang5840@gmail.com>
2021-04-25 09:57:46 +0530

    confiure socket.io

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ const flash =  require('connect-flash');
 // middleware to store flash messages
 const customMware = require('./config/middleware');
 
+// setup chat server to use with sockets.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("Chat server is listening on port 5000");
+
 // layouts
 app.use(expressLayouts);
 
