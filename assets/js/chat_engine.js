501164b57ae5d5d2c59c2c5cba3b546bd2058e05
Aman Gupta <amang5840@gmail.com>
2021-04-25 11:04:44 +0530

    create and join chat room

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -10,8 +10,18 @@ class ChatEngine{
     }
 
     connectionHandler(){
+        let self = this;
         this.socket.on('connect', function(){
             console.log('Connection established using sockets...!');
+
+            self.socket.emit('join_room',{
+                user_email: self.userEmail,
+                chat_room: 'nocial'
+            });
+
+            self.socket.on('user_joined', function(data){
+                console.log('A new user joined: ', data);
+            });
         })
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -11,5 +11,13 @@ module.exports.chatSockets = function(socketServer){
         socket.on('disconnect', function(){
             console.log('Socket disconnected!');
         });
+
+        socket.on('join_room', function(data){
+            console.log('Joining request received: ', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatrom).emit('user_joined', data);
+        });
     });
 };
